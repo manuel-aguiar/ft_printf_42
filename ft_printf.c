a94a40cbbdcc4088a916fdf20783023b6ef7095c
manuel-aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2023-04-13 20:20:19 +0100

    Create ft_printf.c

---
+++ ft_printf.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: manuel <manuel@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/22 19:03:55 by manuel            #+#    #+#             */
+/*   Updated: 2023/03/22 19:03:55 by manuel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int    flagcheck(char c, char *flags, va_list ptr)
+{
+    if (!flags)
+        return (0);
+    while (*flags)
+    {
+        if (c == *flags)
+            return (1);
+        flags++;
+    }
+    va_arg(ptr, int);
+    return (0);
+}
+
+int	printformat(va_list ptr, char flag)
+{
+    if (!flagcheck(flag, FLAGS, ptr))
+        return (0);
+	if (flag == 'c')
+		return (putcountchar(va_arg(ptr, int)));
+	if (flag == 's')
+		return (putcountstr(va_arg(ptr, char *)));
+	if (flag == 'p')
+		return (putcountaddr(va_arg(ptr, void *)));
+	if (flag == 'd' || flag == 'i')
+		return (putcountint(va_arg(ptr, int)));
+	if (flag == 'u')
+		return (putcountunint(va_arg(ptr, unsigned int)));
+	if (flag == 'x')
+		return (putcounthex(va_arg(ptr, unsigned int), HEXS));
+	if (flag == 'X')
+		return (putcounthex(va_arg(ptr, unsigned int), HEXB));
+	if (flag == '%')
+		return (putcountchar('%'));
+}
+
+int	ft_printf(const char *str, ...)
+{
+	va_list	ptr;
+	int		i;
+
+	i = 0;
+	va_start(ptr, str);
+	while (*str)
+	{
+		if (*str != '%')
+		{
+			write(1, str, 1);
+			i++;
+		}
+		else
+		{
+			i += printformat(ptr, *(str + 1));
+			str++;
+		}
+		str++;
+	}
+	va_end(ptr);
+	return (i);
+}
